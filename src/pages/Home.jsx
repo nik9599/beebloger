e9fdfdc3f0d15c01a0aee532304d4e3bd383fc92
nik9599 <nikhil9599094257@gmail.com>
2022-12-04 20:43:21 +0530

    04-12-2022

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../pages/Home.css';
+
+function Home() {
+    return (
+        <div className='Home' >
+            <div className='home-title'><h1>Update The World With New Tech</h1></div>
+
+        </div>
+    );
+}
+
+export default Home;
+
