49eadfe5f9aad68fa0b66633e81ad43fab0df3f8
nik9599 <nikhil9599094257@gmail.com>
2022-12-06 23:36:28 +0530

    date 6-12 -2022

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -4,7 +4,24 @@ import './write.css'
 export default function Write() {
     return (
         <div className='Write'>
-           
+            <form className="writeform">
+                <div className="writeformgroup">
+                    <label htmlFor="fileinput" className="fileinput">
+                        <i class="fa-solid fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileinput" style={{ display: 'none' }} />
+                    <input type='text' placeholder='Enter the title' autoFocus={true}
+                        className='writeinput' />
+                </div>
+
+                <div className="writeformgroup">
+                    <textarea placeholder='Tell Your Story' type='text' className='writeinput writetext' />
+                
+                </div>
+               
+                <button className="writesubmit">Publish</button>
+
+            </form>
         </div>
     )
 }
